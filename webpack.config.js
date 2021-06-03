3064dd0a7391efad50f0f4a3b91379d43a28fb78
claire ogesen <claire.ogesen.co@gmail.com>
2021-06-02 17:17:08 -0700

    kind of working

--- webpack.config.js
+++ webpack.config.js
@@ -1,51 +1,70 @@
-//We'll use a core Node.js module that we've used previously, called path. 
-//Remember, webpack uses Node.js to build our application, so we can use npm modules and the require module.
-const webpack = require("webpack")
+const webpack = require('webpack');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer')
+  .BundleAnalyzerPlugin;
+// const SWPrecacheWebpackPlugin = require("sw-precache-webpack-plugin");
+// const WebpackPwaManifest = require("webpack-pwa-manifest");
 const path = require('path');
-const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 
-//For a basic configuration, we need to provide webpack with three properties: entry, output, and mode.
-module.exports = {
+const config = {
   entry: {
-    app: "./assets/js/script.js",
-    events: "./assets/js/events.js",
-    schedule: "./assets/js/schedule.js",
-    tickets: "./assets/js/tickets.js"
+    app: './assets/js/script.js',
+    events: './assets/js/events.js',
+    schedule: './assets/js/schedule.js',
+    tickets: './assets/js/tickets.js'
   },
   output: {
-    filename: "[name].bundle.js",
-    path: __dirname + "/dist",
+    filename: '[name].bundle.js',
+    path: __dirname + '/dist'
   },
   module: {
     rules: [
       {
-        test: /\.jpg$/i,
+        test: /\.(png|jpe?g|gif)$/i,
         use: [
           {
             loader: 'file-loader',
             options: {
               esModule: false,
               name(file) {
-                return "[path][name].[ext]"
+                return '[path][name].[ext]';
               },
-              publicPath: function (url) {
-                return url.replace("../", "/assets/")
+              publicPath(url) {
+                return url.replace('../', '/assets/');
               }
             }
           },
-          loader: 'image-webpack-loader'
+          {
+            loader: 'image-webpack-loader'
+          }
         ]
       }
     ]
   },
   plugins: [
     new webpack.ProvidePlugin({
-      $: "jquery",
-      jQuery: "jquery"
+      $: 'jquery',
+      jQuery: 'jquery'
     }),
     new BundleAnalyzerPlugin({
-      analyzerMode: "static",  // the report outputs to an HTML file in the dist folder
+      analyzerMode: 'static'
     })
+    // new WebpackPwaManifest({
+    //   name: "Food Event",
+    //   short_name: "Foodies",
+    //   description: "An app that allows you to view upcoming food events.",
+    //   background_color: "#01579b",
+    //   theme_color: "#ffffff",
+    //   fingerprints: false,
+    //   inject: false,
+    //   icons: [{
+    //     src: path.resolve("assets/img/icons/icon-512x512.png"),
+    //     sizes: [96, 128, 192, 256, 384, 512],
+    //     destination: path.join("assets", "icons")
+    //   }]
+    // })
   ],
-  mode: "development"
+  mode: 'development'
 };
+
+module.exports = config;
+
